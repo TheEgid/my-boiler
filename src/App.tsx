c6868513991f6636ce8c11aadda00e5bd030bb08
TheEgid <egid.f1@gmail.com>
2022-12-24 10:43:09 +0300

    Fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import logo from "./assets/logo.svg"
-import "./App.css";
+import React from 'react'
+import logo from './assets/logo.svg'
+import './App.css'
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+
+
+            <header 
+            className=
+
+
+            "App-header">
+
+                <img src=  {logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
@@ -19,7 +26,7 @@ function App() {
                 </a>
             </header>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
