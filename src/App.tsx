83b96b98f811abb35803c91e191a88f5b05b8b4e
TheEgid <egid.f1@gmail.com>
2022-12-24 13:48:09 +0300

    Fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from "react";
-import logo from "./assets/logo.svg";
+import { Container } from "react-bootstrap";
+import Spinner from "./components/common/Spinner";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <div className="App">
+                <Container>
+                    <Spinner />
+                </Container>
+            </div>
+        </>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import store, { persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
 import App from "./App";
-
 import "./index.scss";
-import "./font-open-sans-condensed.scss";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("root")!); // notice the '!'
 
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <App />
-            </PersistGate>
-        </Provider>
+        <App />
     </React.StrictMode>
 );
